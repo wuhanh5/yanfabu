c0f49249fb19ada590915150b19eb3ca4ae7bc47
windole <1322670109@qq.com>
2016-10-31 10:56:39 +0800

    second
    第二次提交

--- js/controllers.js
+++ js/controllers.js
@@ -5,9 +5,28 @@ routerApp.controller('colorCtrl',function($scope){
         }
 })
 
-routerApp.controller('homeCtrl',['$scope','homeService',function($scope,homeService){
+routerApp.controller('bannerCtrl',['$scope','homeService','$timeout',function($scope,homeService,$timeout){
 	homeService.get().success(function(res){
 		console.log(res.data);
 		$scope.arr =res.data.homePageInfo.topBanner;
 	})
+	$timeout(function(){
+		$scope.swiper = new Swiper('.swiper-container', {
+			pagination: '.swiper-pagination',
+			paginationClickable: true,
+			centeredSlides: true,
+			autoplay: 2500,
+			autoplayDisableOnInteraction: false,
+			loop : true
+		});
+	},100);
+	
+	
+}]);
+
+routerApp.controller('homeMainCtrl',['$scope','homeService',function($scope,homeService){
+	homeService.get().success(function(res){
+		$scope.citys =res.data.homePageInfo.recommendCity;
+	})
 }])
+
